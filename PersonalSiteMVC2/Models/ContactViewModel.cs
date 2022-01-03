81a3bf0dc05cb762f7cea5cabec17542c38841b4
dommperez <dperez20002@outlook.com>
2022-01-03 17:26:49 -0600

    new template

---
+++ PersonalSiteMVC2/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC2.Models
+{
+    public class ContactViewModel
+    {
+       [Required(ErrorMessage = "**Please provide a name**")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "**Please provide a email**")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "**Please provide a message**")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
